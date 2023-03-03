7be36a901ee97fe6c0900fc9ad07c81a48e07b58
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2023-03-03 20:14:50 +0200

    modified routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,72 @@
-import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
-import Login from "../views/Login.vue";
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import Users from '../components/Users.vue'
+import Dashboard from '../components/Dashboard.vue'
+import store from '../store'
 
 const routes = [
-    {
-        path: "/",
-        name: "home",
-        component: Home,
+  {
+    path: '/login',
+    name: 'login',
+    component: Login,
+  },
+  {
+    path: '/',
+    component: Home,
+    meta: {
+      requiresAuth: true,
     },
-    {
-        path: "/login",
-        name: "login",
-        component: Login,
-        
-    }
+    children: [
+      {
+        path: '/',
+        name: 'dashboard',
+        component: Dashboard,
+        meta: {
+          requiresAuth: true,
+        },
+      },
+      {
+        path: 'users',
+        name: 'users',
+        component: Users,
+        meta: {
+          requiresAuth: true,
+        },
+      },
+    ],
+  },
 
 ]
 
 const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL),
-    routes,
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+})
+
+router.beforeEach((to, from, next) => {
+  //update store
+  const user = JSON.parse(localStorage.getItem('user'))
+  if (user) {
+    store.commit('SET_USER', user)
+  }
+
+  //check auth
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    //check if user is logged in
+    if (!store.getters.isLoggedIn) {
+      next('/login')
+    } else {
+      next()
+    }
+  } else {
+    //if not requires auth, redirect to home if logged in
+    if (store.getters.isLoggedIn) {
+      next('/')
+    } else {
+      next()
+    }
+  }
 })
 
 export default router
