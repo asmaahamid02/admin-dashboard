4ec012a0a389b1e97ffeebe74b12d29d8b87ad9c
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2023-03-03 20:15:23 +0200

    added vuetify and data table

--- src/main.js
+++ src/main.js
@@ -1,19 +1,38 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
+import Vue3EasyDataTable from 'vue3-easy-data-table';
+import 'vue3-easy-data-table/dist/style.css';
 
 // Vuetify
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
+import { VDataTable } from 'vuetify/labs/VDataTable'
 
 const vuetify = createVuetify({
-  components,
+  components:{
+    ...components,
+    VDataTable
+  },
   directives,
+  icons: {
+    defaultSet: 'mdi',
+  },
+  defaults: {
+    VDataTable: {
+      fixedHeader: true,
+      noDataText: 'Results not found',
+    },
+  },
 })
 
 createApp(App)
 .use(vuetify)
 .use(router)
+.use(store)
+.component('EasyDataTable', Vue3EasyDataTable)
 .mount('#app')
