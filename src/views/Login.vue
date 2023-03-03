8265970fbdc2f9a39948b15b5f3bcdc3d7b7878f
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2023-03-03 20:16:18 +0200

    modified login view

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,93 +1,98 @@
 <template>
-   <v-sheet 
-   width="400" 
-   rounded
-    elevation="10"
+  <v-sheet width="400" rounded elevation="10" class="mx-auto pa-12">
+    <h1 class="text-center mb-5 text-purple">Login</h1>
 
-   class="mx-auto pa-12"
-   >
-   <h1 class="text-center mb-5 text-purple">Login</h1>
-   
     <!--Form-->
-    <v-form>
-        <v-text-field 
-        label="Username"  
-        :rules="usernameRules"      
-        :v-model="username"
-        ></v-text-field>
+    <v-form @submit.prevent="submitForm" ref="form">
+      <v-text-field
+        label="E-mail"
+        :rules="emailRules"
+        v-model="email"
+      ></v-text-field>
 
-        <v-text-field
+      <v-text-field
         type="password"
         label="Password"
-        :v-model="password"
+        v-model="password"
         :rules="passwordRules"
         class="mt-5"
-        >
-    </v-text-field>
-    <v-btn 
-    type="submit"
-     block 
-     color="purple"
-     class="mt-5"
-     >Login</v-btn>
+      >
+      </v-text-field>
+      <v-btn type="submit" block color="purple" class="mt-5">Login</v-btn>
     </v-form>
-   </v-sheet>
+  </v-sheet>
 </template>
 
 <script>
-    export default {
-        name: 'Login',
-        data(){
-            return{
-                username: '',
-                password: '',
-                usernameRules:[
-                    value => {
-                        //check length
-                        if(value?.length < 5) return 'Username must be at least 5 characters'
+export default {
+  name: 'Login',
+  data() {
+    return {
+      valid: false,
+      email: '',
+      password: '',
+      emailRules: [
+        (value) => {
+          //check length
+          if (!value) return 'E-mail is required'
 
-                        //check for spaces
-                        if(value?.includes(' ')) return 'Username cannot contain spaces'
+          //check valid email
+          if (!value.match(/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/))
+            return 'E-mail must be valid'
 
-                        //check for special characters
-                        if(!value?.match(/^[a-zA-Z0-9]+$/)) return 'Username cannot contain special characters'
+          return true
+        },
+      ],
+      passwordRules: [
+        (value) => {
+          //check length
+          if (value?.length < 8) return 'Password must be at least 8 characters'
 
-                        //check if starts with number
-                        if(value?.match(/^[0-9]/)) return 'Username cannot start with a number'
+          //check for spaces
+          if (value?.includes(' ')) return 'Password cannot contain spaces'
 
-                        //check if no letters
-                        if(!value?.match(/[a-zA-Z]/)) return 'Username must contain at least one letter'
+          //check for uppercase
+          if (!value?.match(/[A-Z]/))
+            return 'Password must contain at least one uppercase letter'
 
-                        return true                    
-                     }
-                ],
-                passwordRules: [
-                    value => {
-                        //check length
-                        if(value?.length < 8) return 'Password must be at least 8 characters'
+          //check for lowercase
+          if (!value?.match(/[a-z]/))
+            return 'Password must contain at least one lowercase letter'
 
-                        //check for spaces
-                        if(value?.includes(' ')) return 'Password cannot contain spaces'
+          //check for number
+          if (!value?.match(/[0-9]/))
+            return 'Password must contain at least one number'
 
-                        //check for uppercase
-                        if(!value?.match(/[A-Z]/)) return 'Password must contain at least one uppercase letter'
-
-                        //check for lowercase
-                        if(!value?.match(/[a-z]/)) return 'Password must contain at least one lowercase letter'
-
-                        //check for number
-                        if(!value?.match(/[0-9]/)) return 'Password must contain at least one number'
-                        
-                        return true                    
-                     }
-                ]
-
-            }
-        }
-        
+          return true
+        },
+      ],
     }
+  },
+  methods: {
+    async validate() {
+      const { valid } = await this.$refs.form.validate()
+      this.valid = valid
+    },
+    async submitForm() {
+      await this.validate()
+      if (!this.valid) return
+
+      this.$store.dispatch('login', {
+        email: this.email,
+        password: this.password,
+      })
+
+      //check if user is logged in
+      if (this.$store.getters.isLoggedIn) {
+        this.$router.push('/')
+      } else if (this.$store.getters.loginError) {
+        alert(this.$store.getters.loginError)
+      } else {
+        alert('Something went wrong')
+      }
+    },
+  },
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style scoped></style>
