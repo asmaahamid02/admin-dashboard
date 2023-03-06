b8c9f1eb4da94344a418b2b23c03245eec301bce
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2023-03-06 19:11:45 +0200

    added mutaion/actions/getter to handle the fields of dynamic form

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,17 @@ const store = createStore({
   state: {
     user: null,
     loginError: null,
+    formSchema: {
+      fields:[
+        {
+          id: 1,
+          inputType: 'Text',
+          inputLabel: '',
+          inputPlaceholder: '',
+          inputRequired: false,
+        }
+      ]
+    },
   },
   mutations: {
     SET_USER(state, user) {
@@ -13,6 +24,9 @@ const store = createStore({
     },
     SET_LOGIN_ERROR(state, error) {
       state.loginError = error
+    },
+    SET_FORM_SCHEMA(state, schema) {
+      state.formSchema = schema
     },
   },
   actions: {
@@ -33,10 +47,24 @@ const store = createStore({
         token: Math.random().toString(36).slice(2),
       })
     },
+    addFormField({}, field) {
+      this.state.formSchema.fields.push(field)
+    },
+    removeFormField({}, id) {
+      const field = this.state.formSchema.fields.find(f => f.id === id)
+      const index = this.state.formSchema.fields.indexOf(field)
+      const newFields = this.state.formSchema.fields.splice(index, 1)
+      const newSchema = {
+        fields: newFields
+      }
+
+      this.commit('SET_FORM_SCHEMA', newSchema)
+    },    
   },
   getters: {
     isLoggedIn: (state) => !!state.user,
     loginError: (state) => state.loginError,
+    formFields: (state) => state.formSchema.fields,
   },
 })
 
