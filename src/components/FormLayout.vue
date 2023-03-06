0b3ae57bc37f6b1f3b080dcbec6b574f6d9b1675
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2023-03-06 19:10:08 +0200

    added form layout component

---
+++ src/components/FormLayout.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container>
+    <v-card elevation="2" width="700" class="mx-auto">
+      <v-card-title class="font-weight-bold text-uppercase">
+      <slot name="title"></slot>
+      </v-card-title>  
+      <v-card-subtitle class="font-weight-medium">
+        <slot name="subtitle"></slot>
+      </v-card-subtitle>
+      <v-card-text>
+   <slot name="content"></slot>
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+    export default {
+        name: 'FormLayout'
+    }
+</script>
