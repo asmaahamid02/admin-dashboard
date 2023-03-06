ab81c9d98c257da9761acef22a0dbbc0a0527726
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2023-03-06 19:12:26 +0200

    added form generator to the dashboard

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,40 +1,41 @@
 <template>
-
-<Header />
-    <v-row
-        align="start"
-        no-gutters
-        style="height: 150px;"
-        class="mt-5 px-2"
-      >
-    <Card  
-    v-for="card in cards"
-    :key="card.title"
-    :title="card.title"
-    :subtitle="card.subtitle"
-    :icon="card.icon"
-    :iconColor="card.iconColor"
-    :value="card.value" 
-    :isPositive="card.isPositive"
-    :btnColor="card.btnColor"
+  <Header />
+  <v-row align="start" no-gutters style="height: 150px" class="mt-5 px-2">
+    <Card
+      v-for="card in cards"
+      :key="card.title"
+      :title="card.title"
+      :subtitle="card.subtitle"
+      :icon="card.icon"
+      :iconColor="card.iconColor"
+      :value="card.value"
+      :isPositive="card.isPositive"
+      :btnColor="card.btnColor"
     />
-    
   </v-row>
   <v-divider></v-divider>
+<!--Generated Form View-->
+<GeneratedForm />
+<v-divider></v-divider>
+  <!--Dynamic form generator-->
+  <FormGenerator />
 </template>
 
 <script>
 import Card from './Card.vue'
 import Header from './Header.vue'
+import FormGenerator from './FormGenerator.vue'
+import GeneratedForm from './GeneratedForm.vue'
 export default {
   name: 'Dashboard',
-  components:{
+  components: {
     Card,
-    Header
-  },
-  data(){
-    return {
-    cards:[
+    Header,
+    FormGenerator,
+    GeneratedForm
+},
+  setup() {
+    const cards = [
       {
         title: 'users',
         subtitle: '12,500',
@@ -67,7 +68,10 @@ export default {
         value: '65%',
         btnColor: 'purple-lighten-4',
       },
-    ],}
-  }
+    ]
+
+
+    return { cards }
+  },
 }
 </script>
